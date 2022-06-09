b0ace1f0fccecc12c54186dca58a03eab4e9459d
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2022-06-10 00:28:28 +0530

    Preorder Traversal of n-ary tree

---
+++ Tree/PreorderTraversal.java
@@ -0,0 +1,58 @@
+package com.dataStructure.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreorderTraversal {
+    static class Node {
+        public int val;
+        public List<Node> children = new ArrayList<>();
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.children.add(new Node(2));
+        root.children.add(new Node(3));
+        root.children.add(new Node(4));
+        root.children.add(new Node(5));
+
+        root.children.get(1).children.add(new Node(6));
+        root.children.get(1).children.add(new Node(7));
+        root.children.get(2).children.add(new Node(8));
+        root.children.get(3).children.add(new Node(9));
+        root.children.get(3).children.add(new Node(10));
+
+        root.children.get(1).children.get(1).children.add(new Node(11));
+        root.children.get(2).children.get(0).children.add(new Node(12));
+        root.children.get(3).children.get(0).children.add(new Node(13));
+
+        root.children.get(1).children.get(1).children.get(0).children.add(new Node(14));
+
+        inOrder(root);
+    }
+
+    private static void inOrder(Node node) {
+        if(node == null)
+            return;
+        int size = node.children.size();
+        for(int i = 0; i < size-1; i++)
+            inOrder(node.children.get(i));
+
+        System.out.print(node.val+" ");
+
+        if(size > 0)
+        inOrder(node.children.get(size-1));
+    }
+}
